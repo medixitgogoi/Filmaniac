a61139276a4bc8804c9d045ae14a36e8cb1a5f1b
Dixit Gogoi <dixitgogoi2017@gmail.com>
2023-08-26 19:23:46 +0530

    Trailer added

---
+++ src/pages/details/Details.jsx
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../../hooks/useFetch";
+import DetailsBanner from "./detailsBanner/DetailsBanner";
+import "./style.scss";
+
+const Details = () => {
+
+  const {mediaType, id} = useParams();
+  const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
+  const { data: credits, loading: creditsLoading } = useFetch(`/${mediaType}/${id}/credits`);
+  
+  return (
+    <div>
+      <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+    </div>
+  );
+}
+
+export default Details;
