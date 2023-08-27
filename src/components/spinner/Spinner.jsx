719222385ef1cc4cfad783935d6774887f81febf
Dixit Gogoi <dixitgogoi2017@gmail.com>
2023-08-28 00:12:29 +0530

    spinner updated

--- src/components/spinner/Spinner.jsx
+++ src/components/spinner/Spinner.jsx
@@ -1,23 +1,21 @@
-import { Oval } from "react-loader-spinner";
+import { Puff } from "react-loader-spinner";
 import "./style.scss";
 
 const Spinner = () => {
-  return (
-      <div className='spinner'>
-          <Oval
-              height={50}
-              width={50}
-              color="#4fa94d"
-              wrapperStyle={{}}
-              wrapperClass=""
-              visible={true}
-              ariaLabel='oval-loading'
-              secondaryColor="#4fa94d"
-              strokeWidth={2}
-              strokeWidthSecondary={2}
-          />
-      </div>
-  );
+    return (
+        <div className='spinner'>
+            <Puff
+                height="80"
+                width="80"
+                radius={1}
+                color="#4fa94d"
+                ariaLabel="puff-loading"
+                wrapperStyle={{}}
+                wrapperClass=""
+                visible={true}
+            />
+        </div>
+    );
 }
 
 export default Spinner;
