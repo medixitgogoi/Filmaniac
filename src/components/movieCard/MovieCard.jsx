caa2befca96fae9bd2e848817eda87e7898a1b54
Dixit Gogoi <dixitgogoi2017@gmail.com>
2023-08-27 21:01:22 +0530

    search functionality added

---
+++ src/components/movieCard/MovieCard.jsx
@@ -0,0 +1,36 @@
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import Img from "../lazyLoadImage/Img";
+import CircleRating from "../circleRating/CircleRating";
+import PosterFallback from "../../assets/no-poster.png";
+import dayjs from "dayjs";
+import "./style.scss";
+
+const MovieCard = ({ data, mediaType }) => {
+    const { url } = useSelector((state) => state.home);
+    const navigate = useNavigate();
+    const posterUrl = data.poster_path
+        ? url.poster + data.poster_path
+        : PosterFallback;
+    return (
+        <div
+            className="movieCard"
+            onClick={() =>
+                navigate(`/${data.media_type || mediaType}/${data.id}`)
+            }
+        >
+            <div className="posterBlock">
+                <Img className="posterImg" src={posterUrl} />
+                <CircleRating rating={data.vote_average.toFixed(1)} />
+            </div>
+            <div className="textBlock">
+                <span className="title">{data.title || data.name}</span>
+                <span className="date">
+                    {dayjs(data.release_date).format("MMM D, YYYY")}
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default MovieCard;
--- src/pages/searchResult/SearchResult.jsx
+++ src/pages/searchResult/SearchResult.jsx
@@ -1,10 +1,89 @@
-import React from 'react';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchDataFromApi } from "../../utils/api";
+import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import noResults from "../../assets/no-results.png";
+import MovieCard from "../../components/movieCard/MovieCard";
+import Img from "../../components/lazyLoadImage/Img";
 import "./style.scss";
 
 const SearchResult = () => {
+
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [pageNum, setPageNum] = useState(1);
+    const { query } = useParams();
+
+    const fetchInitialData = () => {
+        setLoading(true);
+        fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then((res) => {
+            setData(res);
+            setPageNum((prev) => prev + 1);
+            setLoading(false);
+        })
+    }
+
+    const fetchNextPageData = () => {
+        fetchDataFromApi(`/search/multi?query=${query}&page=${pageNum}`).then(
+            (res) => {
+                if (data?.results) {
+                    setData({
+                        ...data,
+                        results: [...data?.results, ...res.results],
+                    });
+                } else {
+                    setData(res);
+                }
+                setPageNum((prev) => prev + 1);
+            }
+        );
+    };
+
+    useEffect(() => {
+        setPageNum(1);
+        fetchInitialData();
+    }, [query])
+
     return (
-        <div>
-            SearchResult
+        <div className="searchResultsPage">
+            {loading && <div style={{ height: "700px", fontSize: "25px", color: "white", textAlign: "center" }}>Loading search page...</div>}
+            {!loading && (
+                <ContentWrapper>
+                    {data?.results?.length > 0 ? (
+                        <>
+                            <div className="pageTitle">
+                                {`Search ${data?.total_results > 0 ? "results" : "result"} for '${query}'`}
+                            </div>
+
+                            <InfiniteScroll
+                                className="content"
+                                dataLength={data?.results?.length}
+                                next={fetchNextPageData}
+                                hasMore={pageNum <= data?.total_pages}
+                                loader={<div style={{height: "100vh"}}>Loading infinite scroll...</div>}
+                            >
+
+                                {data?.results?.map((item, index) => {
+
+                                    if (item.media_type === "person") return;
+
+                                    return (
+                                        <MovieCard key={index} data={item} />
+                                    )
+
+                                })}
+                            </InfiniteScroll>
+
+                        </>
+                    ) : (
+                        <div className="noResultsPage">
+                            <span>Sorry! No Results Found</span>
+                            <Img className="noResultImg" src={noResults} />
+                        </div>
+                    )}
+                </ContentWrapper>
+            )}
         </div>
     );
 }
